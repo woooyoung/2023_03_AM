affa614f14ab79d16342dab3a5bfdf946676559e
woooyoung <axdsw121@gmail.com>
2023-03-08 17:42:27 +0900

    게시물 작성 기능 추가

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -8,12 +8,37 @@ public class Main {
 
 		Scanner sc = new Scanner(System.in);
 
+		int lastArticleId = 0;
+
 		while (true) {
 			System.out.print("명령어 > ");
 			String command = sc.nextLine();
-			
-			if(command.equals("exit")) {
+
+			if (command.length() == 0) {
+				System.out.println("명령어를 입력해주세요");
+				continue;
+			}
+
+			if (command.equals("exit")) {
 				break;
+			}
+
+			if (command.equals("article list")) {
+				System.out.println("게시글이 없습니다");
+			} else if (command.equals("article write")) {
+				int id = lastArticleId + 1;
+				System.out.print("제목 : ");
+				String title = sc.nextLine();
+				System.out.print("내용 : ");
+				String body = sc.nextLine();
+
+//				System.out.println(title + " / " + body);
+
+				System.out.printf("%d번글이 생성되었습니다\n", id);
+				lastArticleId++;
+
+			} else {
+				System.out.println("존재하지 않는 명령어입니다");
 			}
 		}
 
