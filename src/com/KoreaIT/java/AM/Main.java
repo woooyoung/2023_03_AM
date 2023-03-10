f228140b84a8e9bb76cf1a2901d2bef290b6523d
woooyoung <axdsw121@gmail.com>
2023-03-10 13:29:01 +0900

    게시글 수정 기능 및 조회수 구현

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -31,10 +31,10 @@ public class Main {
 				if (articles.size() == 0) {
 					System.out.println("게시글이 없습니다");
 				} else {
-					System.out.println(" 번호  //  제목  ");
+					System.out.println(" 번호  //  제목    //  조회  ");
 					for (int i = articles.size() - 1; i >= 0; i--) {
 						Article article = articles.get(i);
-						System.out.printf("  %d   //   %s  \n", article.id, article.title);
+						System.out.printf("  %d   //   %s   //   %d  \n", article.id, article.title, article.hit);
 					}
 				}
 
@@ -46,7 +46,7 @@ public class Main {
 				System.out.print("내용 : ");
 				String body = sc.nextLine();
 
-				Article article = new Article(id, regDate, title, body);
+				Article article = new Article(id, regDate, regDate, title, body);
 				articles.add(article);
 
 				System.out.printf("%d번글이 생성되었습니다\n", id);
@@ -77,10 +77,52 @@ public class Main {
 					System.out.printf("%d번 게시물은 존재하지 않습니다\n", id);
 					continue;
 				}
+
+				foundArticle.hit++;
+
 				System.out.println("번호 : " + foundArticle.id);
-				System.out.println("날짜 : " + foundArticle.regDate);
+				System.out.println("작성날짜 : " + foundArticle.regDate);
+				System.out.println("수정날짜 : " + foundArticle.updateDate);
 				System.out.println("제목 : " + foundArticle.title);
 				System.out.println("내용 : " + foundArticle.body);
+				System.out.println("조회 : " + foundArticle.hit);
+
+			} else if (command.startsWith("article modify")) {
+
+				String[] cmdDiv = command.split(" ");
+
+				if (cmdDiv.length < 3) {
+					System.out.println("명령어를 확인해주세요");
+					continue;
+				}
+
+				int id = Integer.parseInt(cmdDiv[2]);
+
+				Article foundArticle = null;
+
+				for (int i = 0; i < articles.size(); i++) {
+					Article article = articles.get(i);
+					if (article.id == id) {
+						foundArticle = article;
+						break;
+					}
+				}
+
+				if (foundArticle == null) {
+					System.out.printf("%d번 게시물은 존재하지 않습니다\n", id);
+					continue;
+				}
+				System.out.print("새 제목 : ");
+				String updateDate = Util.getNowDateTimeStr();
+				String newTitle = sc.nextLine();
+				System.out.print("새 내용 : ");
+				String newBody = sc.nextLine();
+
+				foundArticle.title = newTitle;
+				foundArticle.body = newBody;
+				foundArticle.updateDate = updateDate;
+
+				System.out.println(id + "번 글을 수정했습니다");
 
 			} else if (command.startsWith("article delete")) {
 
@@ -124,13 +166,17 @@ public class Main {
 class Article {
 	int id;
 	String regDate;
+	String updateDate;
 	String title;
 	String body;
+	int hit;
 
-	Article(int id, String regDate, String title, String body) {
+	Article(int id, String regDate, String updateDate, String title, String body) {
 		this.id = id;
 		this.regDate = regDate;
+		this.updateDate = updateDate;
 		this.title = title;
 		this.body = body;
+		this.hit = 0;
 	}
 }
